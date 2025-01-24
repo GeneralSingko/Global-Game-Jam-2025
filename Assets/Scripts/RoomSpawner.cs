c972544eb850cdb2df100557520651f3f4bee1f1
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-01-24 22:36:55 +0800

    RoomSpawner Script

---
+++ Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+
+    private void Update()
+    {
+        if (openingDirection == 1)
+        {
+
+        }
+        else if (openingDirection == 2)
+        {
+
+        }
+        else if (openingDirection == 3)
+        {
+
+        }
+        else if (openingDirection == 4) 
+        {
+            
+        }
+    }
+}
