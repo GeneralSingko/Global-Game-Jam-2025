7b749738b53bc38d806def7cddd65e1cbe178e8b
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-01-25 15:29:42 +0800

    refined movement and player health

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,4 +1,3 @@
-/*using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -16,43 +15,6 @@ public class Bullet : MonoBehaviour
     private void Update()
     {
         transform.Translate(Vector3.up * bulletSpeed * Time.deltaTime);
-    }
-
-    /*private void OnTriggerEnter(Collider other) // Use OnTriggerEnter2D for 2D
-    {
-        // Check for collision and return to pool
-        if (other.CompareTag("EraseBullets"))
-        {
-            ReturnToPool();
-        }
-    }*/
-
-using UnityEngine;
-
-public class Bullet : MonoBehaviour
-{
-    [Header("Bullet Speed")]
-    [SerializeField] private float bulletSpeed;
-    [SerializeField] private float lifespan = 5f;
-
-    private void OnEnable()
-    {
-        // Automatically return to the pool after a set lifespan
-        Invoke(nameof(ReturnToPool), lifespan);
-    }
-
-    private void Update()
-    {
-        transform.Translate(Vector3.up * bulletSpeed * Time.deltaTime);
-    }
-
-    private void OnTriggerEnter(Collider other) // Use OnTriggerEnter2D for 2D
-    {
-        // Check for collision and return to pool
-        if (other.CompareTag("EraseBullets"))
-        {
-            ReturnToPool();
-        }
     }
 
     public void ReturnToPool()
